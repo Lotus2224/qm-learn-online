e4eb823f0e353498dcddfa0105c90069f0cd48a6
itcast <hello@itcast.cn>
2024-06-22 10:08:24 +0800

    提交父工程parent和基础工程base

---
+++ learn-online-base/src/main/java/com/qm/LearnOnlineBaseApplication.java
@@ -0,0 +1,13 @@
+package com.qm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOnlineBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnOnlineBaseApplication.class, args);
+    }
+
+}
